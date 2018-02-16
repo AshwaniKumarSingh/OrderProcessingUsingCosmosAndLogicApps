d3192c7846cf3a11928faadd94a73f46a98b2fba
Ashwani Kumar <ashwani_ru@rediffmail.com>
2018-02-15 19:34:45 -0500

    updated swagger

--- IPM.PO.POC/IPM.PO.POC/Startup.cs
+++ IPM.PO.POC/IPM.PO.POC/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace OrderProcessing.POC
 {
@@ -26,6 +27,10 @@ namespace OrderProcessing.POC
         {
             services.AddMvc();
             services.AddSingleton<IConfiguration>(Configuration);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Order API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +40,16 @@ namespace OrderProcessing.POC
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Order API V1");
+            });
+
+
 
             app.UseMvc();
         }
